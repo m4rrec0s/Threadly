7f7b13930373645410263b4f8c0e411e08d66fc1
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-12-23 14:03:28 -0300

    feat: Implement display of likes and comments on posts associated with each user's post

--- app/components/postsList.tsx
+++ app/components/postsList.tsx
@@ -6,16 +6,21 @@ import Image from "next/image";
 import { dateConvert } from "../helpers/dateConvert";
 import { Button } from "./ui/button";
 import { HeartIcon, MessageCircle, Send } from "lucide-react";
-import { Post } from "../types/Posts";
 import { User } from "../types/Users";
-import { Comment } from "../types/Comments";
-import { Likes } from "../types/Likes";
+import { Post } from "../types/Posts";
+import { Skeleton } from "./ui/skeleton";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const PostsList = () => {
-  const [posts, setPosts] = useState<Post[]>();
-  const [users, setUsers] = useState<User[]>();
-  const [comments, setComments] = useState<Comment[]>();
-  const [likes, setLikes] = useState<Likes[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
@@ -25,14 +30,26 @@ const PostsList = () => {
 
   const getPosts = async () => {
     try {
-      const responsePosts = await axiosClient.get("/posts");
-      const responseUsers = await axiosClient.get("/users");
-      const responseComments = await axiosClient.get("/comments");
-      const responseLikes = await axiosClient.get("/likes");
-      setPosts(responsePosts.data);
-      setUsers(responseUsers.data);
-      setComments(responseComments.data);
-      setLikes(responseLikes.data);
+      const response = await axiosClient.get("/users");
+      const usersData = response.data;
+      setUsers(usersData);
+
+      const allPosts = usersData.flatMap((user: User) =>
+        user.posts.map((post) => ({
+          ...post,
+          user: {
+            id: user.id,
+            name: user.name,
+            image: user.image,
+            username: user.username,
+          },
+        }))
+      );
+      allPosts.sort(
+        (a: Post, b: Post) =>
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+      setPosts(allPosts);
     } catch (error: unknown) {
       setError("Error fetching posts - " + (error as Error).message);
     } finally {
@@ -40,26 +57,45 @@ const PostsList = () => {
     }
   };
 
-  const findUserById = (userId: string | number): User | undefined => {
-    if (!users || users.length === 0) {
-      console.error("Users data not loaded or empty");
-    }
-    return users?.find((user) => String(user.id) === String(userId));
-  };
-
-  const findCommentsByPostId = (postId: string): Comment[] | undefined => {
-    return comments?.filter((comment) => comment.post_id === postId);
-  };
-
-  const findLikesByPostId = (postId: string): Likes[] | undefined => {
-    return likes?.filter((like) => like.post_id === postId);
-  };
+  // const findUserById = (id: string | number) => {
+  //   return users?.find((user) => user.id === id);
+  // };
 
   if (loading) {
     return (
-      <section>
-        <div>
-          <h3>Loading...</h3>
+      <section className="flex-grow">
+        <div className="flex items-center py-6 h-full overflow-y-hidden">
+          <div className="space-y-6 w-full flex flex-col items-center max-w-2xl mx-auto">
+            <div>
+              <div className="flex items-center space-x-2">
+                <Skeleton className="h-7 w-7 rounded-full" />
+                <Skeleton className="h-5 w-24" />
+              </div>
+              <div className="mt-3">
+                <Skeleton className="h-[500px] w-[500px]" />
+
+                <div className="mt-6 space-y-3">
+                  <Skeleton className="h-5 w-[]" />
+                  <Skeleton className="h-5 w-80" />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center space-x-2">
+                <Skeleton className="h-7 w-7 rounded-full" />
+                <Skeleton className="h-5 w-24" />
+              </div>
+              <div className="mt-3">
+                <Skeleton className="h-[500px] w-[500px]" />
+
+                <div className="mt-6 space-y-3">
+                  <Skeleton className="h-5 w-96" />
+                  <Skeleton className="h-5 w-80" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
@@ -67,9 +103,10 @@ const PostsList = () => {
 
   if (error) {
     return (
-      <section>
-        <div>
-          <h3>{error}</h3>
+      <section className="flex-grow flex items-center justify-center">
+        <div className="flex flex-col items-center space-y-4">
+          <Image src="/sadCloud.svg" alt="error" width={200} height={170} />
+          <h3>Ocorreu um erro: {error}</h3>
         </div>
       </section>
     );
@@ -78,105 +115,226 @@ const PostsList = () => {
   return (
     <div className="flex-grow overflow-y-auto py-6">
       <section className="space-y-6 w-full flex flex-col items-center max-w-2xl mx-auto">
-        {posts && posts.length > 0 ? (
-          posts.map((post) => {
-            const user = findUserById(post.author_id);
-            return (
-              <div key={post.id}>
-                {user && (
-                  <div className="flex items-center space-x-2">
-                    <Image
-                      src={`${user.image}?random=${user.id}`}
-                      alt="user image"
-                      width={30}
-                      height={30}
-                      className="rounded-full"
-                    />
-                    <h3 className="font-normal text-base">{user.username}</h3>
-                  </div>
-                )}
-                <div className="mt-3">
-                  <Image
-                    src={`${post.image_url}?random=${post.id}0`}
-                    alt="post image"
-                    width={500}
-                    height={500}
-                  />
-                  <div className="space-x-4">
-                    <Button
-                      variant={"link"}
-                      className="p-0 hover:text-red-500 hover:no-underline"
-                    >
-                      <HeartIcon className="!w-5 !h-5" />
-                      <span>{findLikesByPostId(post.id)?.length}</span>
-                    </Button>
-                    <Button
-                      variant={"link"}
-                      className="p-0 hover:text-blue-500 hover:no-underline"
-                    >
-                      <MessageCircle className="!w-5 !h-5" />
-                    </Button>
-                    <Button
-                      variant={"link"}
-                      className="p-0 hover:text-green-500 hover:no-underline"
-                    >
-                      <Send className="!w-5 !h-5" />
-                    </Button>
-                  </div>
-                  <div className="mt-3">
-                    <p className="text-base">
-                      <span className="text-base font-semibold">
-                        {user?.name}
-                      </span>{" "}
-                      {post.content}
-                    </p>
-                    <span className="text-xs text-gray-500">
-                      {dateConvert(post.created_at).charAt(0).toUpperCase() +
-                        dateConvert(post.created_at).slice(1)}
-                    </span>
-                  </div>
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <div key={post.id}>
+              <div className="flex items-center space-x-2">
+                <Image
+                  src={`${post.user.image}?random=${post.user.id}`}
+                  alt="user image"
+                  width={30}
+                  height={30}
+                  className="rounded-full"
+                />
+                <h3 className="font-normal text-base">{post.user.username}</h3>
+              </div>
+
+              <div className="mt-3">
+                <Image
+                  src={`${post.image_url}?random=${post.id}`}
+                  alt="post image"
+                  width={500}
+                  height={500}
+                />
+                <div className="space-x-4 mt-1">
+                  <Button
+                    variant="link"
+                    className="hover:text-red-500 px-0 hover:no-underline"
+                  >
+                    <HeartIcon className="!w-5 !h-5" />
+                  </Button>
+                  <Button
+                    variant="link"
+                    className="hover:text-blue-500 px-0 hover:no-underline"
+                  >
+                    <MessageCircle className="!w-5 !h-5" />
+                  </Button>
+                  <Button
+                    variant="link"
+                    className="hover:text-green-500 px-0 hover:no-underline"
+                  >
+                    <Send className="!w-5 !h-5" />
+                  </Button>
                 </div>
 
-                <div>
-                  {findCommentsByPostId(post.id)?.map((comment) => {
-                    const user = findUserById(comment.author_id);
-                    return (
-                      <div
-                        key={comment.id}
-                        className="mt-3 flex items-center space-x-2"
+                <Dialog>
+                  <DialogTrigger asChild>
+                    {Array.isArray(post.likes) && post.likes.length > 0 ? (
+                      <Button
+                        variant="link"
+                        className="text-sm font-semibold text-gray-500 hover:text-gray-700 px-0 hover:no-underline"
                       >
-                        <Image
-                          src={`${user?.image}?random=${user?.id}`}
-                          alt="user image"
-                          width={30}
-                          height={30}
-                          className="rounded-full"
-                        />
-                        <div className="flex flex-col">
-                          <p>
-                            <span className="font-semibold text-base">
-                              {user?.username}
-                            </span>{" "}
-                            <span className="text-sm">{comment.content}</span>
-                          </p>
-                          <div>
-                            <span className="text-xs text-gray-500">
-                              {dateConvert(comment.created_at)}
-                            </span>
+                        <div>
+                          {post.likes && post.likes.length > 0 ? (
+                            <div className="flex -space-x-2">
+                              {post.likes.slice(0, 3).map((like, index) => {
+                                const user = users.find(
+                                  (user) => user.id === like.user_id
+                                );
+                                return (
+                                  <Image
+                                    key={index}
+                                    src={`${user?.image}?random=${like.user_id}`}
+                                    alt="user image"
+                                    width={20}
+                                    height={20}
+                                    className="rounded-full border-2 border-black"
+                                    style={{ zIndex: 3 - index }}
+                                  />
+                                );
+                              })}
+                            </div>
+                          ) : null}
+                        </div>
+                        <span>
+                          {post.likes.length > 1
+                            ? `${post.likes.length} curtidas`
+                            : `${post.likes.length} curtida`}
+                        </span>
+                      </Button>
+                    ) : null}
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle className="text-center">
+                        Curtidas
+                      </DialogTitle>
+                    </DialogHeader>
+                    <div className="border-t border-gray-600 flex flex-col space-y-3">
+                      {loading ? (
+                        <div>Loading likes...</div>
+                      ) : post.likes ? (
+                        post.likes.map((like) => {
+                          const user = users.find(
+                            (user) => user.id === like.user_id
+                          );
+                          return (
+                            <div
+                              key={like.id}
+                              className="flex items-center space-x-2"
+                            >
+                              <Image
+                                src={`${user?.image}?random=${like.user_id}`}
+                                alt="user image"
+                                width={30}
+                                height={30}
+                                className="rounded-full"
+                              />
+                              <p className="font-semibold">{user?.username}</p>
+                            </div>
+                          );
+                        })
+                      ) : null}
+                    </div>
+                  </DialogContent>
+                </Dialog>
+
+                <div className="mt-3">
+                  <p className="text-base">
+                    <span className="text-base font-semibold">
+                      {post.user.name}
+                    </span>{" "}
+                    {post.content}
+                  </p>
+                  <span className="text-xs text-gray-500">
+                    {dateConvert(post.created_at)}
+                  </span>
+                </div>
+              </div>
+
+              {/* Comments */}
+              <div>
+                {post && post.comments && post.comments.length > 0 ? (
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button
+                        variant="link"
+                        className="text-sm font-semibold text-gray-500 hover:text-gray-700 px-0 hover:no-underline"
+                      >
+                        Ver todos os {post.comments.length} comentários
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="text-sm">
+                      <DialogHeader className="hidden">
+                        <DialogTitle>Comentários</DialogTitle>
+                        <DialogDescription>
+                          Lista de comentários deste post
+                        </DialogDescription>
+                      </DialogHeader>
+                      <div>
+                        <div className="flex items-center space-x-2">
+                          {post.user && (
+                            <Image
+                              src={`${post.user.image}?random=${post.user.id}`}
+                              alt="user image"
+                              width={30}
+                              height={30}
+                              className="rounded-full"
+                            />
+                          )}
+                          <div className="flex flex-col">
+                            <p>
+                              <span className="font-semibold text-base">
+                                {post.user.username}
+                              </span>{" "}
+                              {post.content}
+                            </p>
                           </div>
                         </div>
+                        <span className="text-xs text-gray-500">
+                          {dateConvert(post.created_at)}
+                        </span>
                       </div>
-                    );
-                  })}
-                </div>
+
+                      <div className="border-b border-gray-600"></div>
+
+                      {post.comments.map((comment) => {
+                        const userComment = users.find(
+                          (user) => user.id === comment.author_id
+                        );
+
+                        return (
+                          <div
+                            key={comment.id}
+                            className="mt-3 flex items-center space-x-2"
+                          >
+                            <Image
+                              src={`${userComment?.image}?random=${comment.author_id}`}
+                              alt="user image"
+                              width={30}
+                              height={30}
+                              className="rounded-full"
+                            />
+                            <div className="flex flex-col">
+                              <p>
+                                <span className="font-semibold text-base">
+                                  {userComment?.username}
+                                </span>{" "}
+                                {comment.content}
+                              </p>
+                              <span className="text-xs text-gray-500">
+                                {dateConvert(comment.created_at)}
+                              </span>
+                            </div>
+                          </div>
+                        );
+                      })}
+                      {post.comments.length === 0 && (
+                        <div className="w-full mt-2 text-center">
+                          <h3 className="text-sm text-gray-400">
+                            Ainda não há nenhum comentário
+                          </h3>
+                        </div>
+                      )}
+                    </DialogContent>
+                  </Dialog>
+                ) : null}
                 <div className="border-b border-gray-600 py-2"></div>
               </div>
-            );
-          })
+            </div>
+          ))
         ) : (
-          <div>
-            <h3>Nenhum post adicionado ainda</h3>
-          </div>
+          <h3>Nenhum post adicionado ainda</h3>
         )}
       </section>
     </div>
