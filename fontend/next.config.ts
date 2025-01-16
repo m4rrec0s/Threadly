86ef49f72a4559923a02fa23b4aa183e950f6b2c
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-01-16 13:51:39 -0300

    feat: Add remotePatterns to localhost

--- fontend/next.config.ts
+++ fontend/next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/**",
+      },
     ],
   },
 };
