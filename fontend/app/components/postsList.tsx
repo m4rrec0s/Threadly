b1378984d968b501056ce4bfc6df2c7970e6ec93
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-01-17 15:40:50 -0300

    feat: add comment creation and like toggle functionality in PostsList component

--- fontend/app/components/postsList.tsx
+++ fontend/app/components/postsList.tsx
@@ -7,12 +7,14 @@ import { User } from "../types/Users";
 import { Post } from "../types/Posts";
 import PostItem from "./PostItem";
 import PostSkeleton from "./PostSkeleton";
+import CreatePostForm from "./CreatePostForm";
 
 const PostsList = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     getPosts();
@@ -32,6 +34,79 @@ const PostsList = () => {
     } finally {
       setLoading(false);
     }
+  };
+
+  const createComment = async (
+    postId: string,
+    authorId: string,
+    content: string
+  ) => {
+    try {
+      const response = await axiosClient.post("/comments", {
+        post_id: postId,
+        author_id: authorId,
+        content: content,
+      });
+      const newComment = response.data;
+
+      setPosts((prevPosts) =>
+        prevPosts.map((post) =>
+          post.id === postId
+            ? { ...post, comments: [...post.comments, newComment] }
+            : post
+        )
+      );
+    } catch (error: unknown) {
+      setError("Error creating comment - " + (error as Error).message);
+    }
+  };
+
+  const toggleLike = async (postId: string, userId: string) => {
+    try {
+      const post = posts.find((post) => post.id === postId);
+      if (!post) return;
+
+      const hasLiked = post.likes.some((like) => like.user_id === userId);
+
+      if (hasLiked) {
+        await axiosClient.delete("/likes", {
+          data: {
+            post_id: postId,
+            user_id: userId,
+          },
+        });
+        setPosts((prevPosts) =>
+          prevPosts.map((post) =>
+            post.id === postId
+              ? {
+                  ...post,
+                  likes: post.likes.filter((like) => like.user_id !== userId),
+                }
+              : post
+          )
+        );
+      } else {
+        const response = await axiosClient.post("/likes", {
+          post_id: postId,
+          user_id: userId,
+        });
+        const newLike = response.data;
+
+        setPosts((prevPosts) =>
+          prevPosts.map((post) =>
+            post.id === postId
+              ? { ...post, likes: [...post.likes, newLike] }
+              : post
+          )
+        );
+      }
+    } catch (error: unknown) {
+      setError("Error toggling like - " + (error as Error).message);
+    }
+  };
+
+  const handlePostCreated = () => {
+    getPosts();
   };
 
   if (loading) {
@@ -55,10 +130,21 @@ const PostsList = () => {
 
   return (
     <div className="">
+      <CreatePostForm
+        onPostCreated={handlePostCreated}
+        open={isModalOpen}
+        onOpenChange={setIsModalOpen}
+      />
       <section className="space-y-6 w-full flex flex-col items-center max-w-3xl mx-auto">
         {posts.length > 0 ? (
           posts.map((post) => (
-            <PostItem key={post.id} post={post} users={users} />
+            <PostItem
+              key={post.id}
+              post={post}
+              users={users}
+              createComment={createComment}
+              toggleLike={toggleLike}
+            />
           ))
         ) : (
           <h3>Nenhum post adicionado ainda</h3>
