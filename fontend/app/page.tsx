20035a64f42a5e50d5dae2b95568590c664b2d9b
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-01-17 15:33:34 -0300

    feat: add profile update listener and improve user avatar handling in Home component

--- fontend/app/page.tsx
+++ fontend/app/page.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import PostsList from "./components/PostsList";
 import { useAuth } from "./context/authContext";
 import { useEffect, useState } from "react";
-import Menu from "./components/Menu";
 import axiosClient from "./services/axiosClient";
 import { User } from "./types/Users";
 import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
@@ -37,50 +36,86 @@ export default function Home() {
       });
   }, [user, router]);
 
+  useEffect(() => {
+    interface ProfileUpdateEvent extends Event {
+      detail: User;
+    }
+
+    const handleProfileUpdate = (event: ProfileUpdateEvent) => {
+      if (event.detail.username === user?.username) {
+        setUserLogged(event.detail);
+      }
+    };
+
+    window.addEventListener(
+      "profileUpdate",
+      handleProfileUpdate as EventListener
+    );
+
+    return () => {
+      window.removeEventListener(
+        "profileUpdate",
+        handleProfileUpdate as EventListener
+      );
+    };
+  }, [user]);
+
   if (!user) {
     return null;
   }
 
   if (loading)
     return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="font-extrabold text-6xl animate-pulse">Threadly</h1>
+      <div className="fixed top-0 left-0 w-screen h-screen bg-background">
+        <div className="flex items-center justify-center h-screen w-full">
+          <h1 className="font-extrabold text-6xl animate-pulse">Threadly</h1>
+        </div>
       </div>
     );
 
   return (
-    <div className="grid grid-cols-[18%_80%] w-screen h-screen relative items-stretch">
-      <div className="col-span-1 fixed top-0 left-0 h-full">
-        <Menu user={userLogged} />
-      </div>
-      <div className="col-span-1 col-start-2 flex justify-center w-full gap-16 py-6 px-2">
-        <PostsList />
-        <section className="flex justify-between items-center h-fit w-96 p-6">
-          <div className="flex items-center gap-2">
-            <Link href={`/${userLogged?.username}`}>
+    <div className="flex justify-center w-full gap-16 py-6 px-2">
+      <PostsList />
+      <section className="flex justify-between items-center h-fit w-96 p-6">
+        <div className="flex items-center gap-2">
+          <Link href={`/${userLogged?.username}`}>
+            {userLogged?.image !== "" ? (
               <Avatar>
-                <AvatarImage src={userLogged?.image} />
+                <AvatarImage
+                  src={`http://localhost:8080/uploads/avatar/${userLogged?.image}`}
+                  className="object-cover"
+                />
                 <AvatarFallback>
                   <div className="flex-grow bg-slate-500 animate-pulse"></div>
                 </AvatarFallback>
               </Avatar>
+            ) : (
+              <Avatar>
+                <AvatarImage
+                  src={"/usuario-sem-foto-de-perfil.jpg"}
+                  className="object-cover"
+                />
+                <AvatarFallback>
+                  <div className="flex-grow bg-slate-500 animate-pulse"></div>
+                </AvatarFallback>
+              </Avatar>
+            )}
+          </Link>
+          <div className="leading-5">
+            <Link href={`/${userLogged?.username}`}>
+              <h3 className="font-semibold">{userLogged?.username}</h3>
             </Link>
-            <div className="leading-5">
-              <Link href={`/${userLogged?.username}`}>
-                <h3 className="font-semibold">{userLogged?.username}</h3>
-              </Link>
-              <h5 className="text-gray-400">{userLogged?.name}</h5>
-            </div>
+            <h5 className="text-gray-400">{userLogged?.name}</h5>
           </div>
-          <Button
-            variant="link"
-            className="text-blue-500 px-4 py-2 rounded-md"
-            onClick={logout}
-          >
-            Mudar
-          </Button>
-        </section>
-      </div>
+        </div>
+        <Button
+          variant="link"
+          className="text-blue-500 px-4 py-2 rounded-md"
+          onClick={logout}
+        >
+          Mudar
+        </Button>
+      </section>
     </div>
   );
 }
