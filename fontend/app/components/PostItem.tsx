4f72f8abdb0e6e130ce0036ac48270288d34c819
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-01-17 15:40:41 -0300

    feat: add dropdown menu with profile and report options in PostItem component

--- fontend/app/components/PostItem.tsx
+++ fontend/app/components/PostItem.tsx
@@ -6,30 +6,95 @@ import PostComments from "./PostComments";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { EllipsisVerticalIcon } from "lucide-react";
+import { useAuth } from "../context/authContext";
 
 interface PostItemProps {
   post: Post;
   users: User[];
+  createComment: (postId: string, authorId: string, content: string) => void;
+  toggleLike: (postId: string, userId: string) => void;
 }
 
-const PostItem: React.FC<PostItemProps> = ({ post, users }) => {
+const PostItem: React.FC<PostItemProps> = ({
+  post,
+  users,
+  createComment,
+  toggleLike,
+}) => {
+  const { user } = useAuth();
+
+  const handleToggleLike = () => {
+    if (user) {
+      toggleLike(post.id, user.id);
+    }
+  };
+
   return (
     <div>
-      <div className="flex items-center space-x-2">
-        <Link href={`/${post.user.username}`}>
-          <Avatar className="h-8 w-8">
-            <AvatarImage
-              src={`${post.user.image}` || "/usuario-sem-foto-de-perfil.jpg"}
-              alt={"user image - " + post.user.id}
-            />
-            <AvatarFallback>
-              <div className="flex-grow bg-slate-500 animate-pulse"></div>
-            </AvatarFallback>
-          </Avatar>
-        </Link>
-        <Link href={`/${post.user.username}`}>
-          <h3 className="font-normal text-base">{post.user.username}</h3>
-        </Link>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <Link href={`/${post.user.username}`}>
+            {post.user?.image !== "" ? (
+              <Avatar className="w-8 h-8">
+                <AvatarImage
+                  src={`http://localhost:8080/uploads/avatar/${post.user?.image}`}
+                  className="object-cover"
+                />
+                <AvatarFallback>
+                  <div className="flex-grow bg-slate-500 animate-pulse"></div>
+                </AvatarFallback>
+              </Avatar>
+            ) : (
+              <Avatar className="w-8 h-8">
+                <AvatarImage
+                  src={"/usuario-sem-foto-de-perfil.jpg"}
+                  className="object-cover"
+                />
+                <AvatarFallback>
+                  <div className="flex-grow bg-slate-500 animate-pulse"></div>
+                </AvatarFallback>
+              </Avatar>
+            )}
+          </Link>
+          <Link href={`/${post.user.username}`}>
+            <h3 className="font-normal text-base">{post.user.username}</h3>
+          </Link>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"link"} className="hover:no-underline p-0">
+              <EllipsisVerticalIcon size={30} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Opções</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href={`/${post.user.username}`} className="w-full py-2">
+                Ver perfil
+              </Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem>
+              <Button
+                variant={"link"}
+                className="w-full text-red-600 py-0 px-0 m-0 text-left justify-start hover:no-underline gap-0"
+              >
+                Denunciar
+              </Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       <div className="mt-3">
@@ -42,7 +107,7 @@ const PostItem: React.FC<PostItemProps> = ({ post, users }) => {
             className="object-cover rounded-lg"
           />
         </div>
-        <PostActions post={post} users={users} />
+        <PostActions post={post} users={users} onLike={handleToggleLike} />
         <div className="mt-3">
           <p className="text-base">
             <span className="text-base font-semibold">{post.user.name}</span>{" "}
@@ -54,7 +119,7 @@ const PostItem: React.FC<PostItemProps> = ({ post, users }) => {
         </div>
       </div>
 
-      <PostComments post={post} users={users} />
+      <PostComments post={post} users={users} createComment={createComment} />
     </div>
   );
 };
